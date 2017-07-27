1ce5e2b92761886a4a7199f1d7cd18ed322f379e
Tim Campbell <timothylevicampbell@gmail.com>
2017-07-27 12:19:24 -0400

    Updates notes and code from lecture

--- 27-trivia/js/index.js
+++ 27-trivia/js/index.js
@@ -2,16 +2,32 @@ let API_BASE_URL = 'https://opentdb.com/api.php?amount=10&type=boolean'
 
 let store = {games: [], questions: []}
 let game_id = 0
-let question_id = 0;
+let question_id = 0
 
 function render(template, selector) {
   $(selector).empty()
   $(selector).html(template)
 }
 
+// The object that is solely concerned with making/handling our request and request logic
+// How do we build the adapter?
+
 $(function() {
-  $('body').on('click', '#new-game', function(event) {
-    fetch(API_BASE_URL)
+  // document.body.addEventListener('click', function(event) {
+  //   if (event.target.id === 'new-game') {
+  //     // Fetch my data
+  //   } else if (event.target.className === 'question-answer') {
+  //     // Answer question
+  //   }
+  // })
+
+  $('body').on('click', '#easy-game', function(event) {
+    // Event delegation
+
+    // Get data from user
+    // Update model / get information
+    // Render
+    fetch(API_BASE_URL + '&difficulty=easy')
       .then(function(response) {
         return response.json()
       })
@@ -20,7 +36,30 @@ $(function() {
       })
   })
 
-  $('body').on('click', '.question-true', function(event) {
+  $('body').on('click', '#medium-game', function(event) {
+    // Event delegation
+    fetch(API_BASE_URL  + '&difficulty=medium')
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        render(new Game(data.results).template(), '#main-page')
+      })
+  })
+
+  $('body').on('click', '#hard-game', function(event) {
+    // Event delegation
+    fetch(API_BASE_URL + '&difficulty=hard')
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        render(new Game(data.results).template(), '#main-page')
+      })
+  })
+
+  $('body').on('click', '.question-answer', function(event) {
+    // Event delegation
     const questionId = parseInt(this.dataset.id)
     const questionVal = !!this.dataset.val
     const question = Question.find(questionId)
@@ -45,7 +84,9 @@ class Game {
     <div class="game" id="game-${this.id}">
       <h1>Game ${this.id}</h1>
       ${this.questions.map(question => question.template()).join("\n")}
-      <button id="new-game">Start New Game</button>
+      <button id="easy-game">Start easy Game</button>
+      <button id="medium-game">Start medium Game</button>
+      <button id="hard-game">Start hard Game</button>
     </div>
     `
   }
@@ -95,8 +136,8 @@ class Question {
         <h3 class="question-category">${this.category}</h3>
         <h4 class="question-difficulty">${this.difficulty}</h4>
 
-        <button class="question-true" data-id="${this.id}" data-val="1">True</button>
-        <button class="question-false" data-id="${this.id}" data-val="0">False</button>
+        <button class="question-answer question-true" data-id="${this.id}" data-val="1">True</button>
+        <button class="question-answer question-false" data-id="${this.id}" data-val="0">False</button>
       </div>
     `
   }
